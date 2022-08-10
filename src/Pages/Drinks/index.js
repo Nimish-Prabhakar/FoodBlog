5044673d83297d1407e0e958aca7bd574596f990
Nishu <nimish.prabhakar@eagleview.com>
2022-08-10 13:44:57 +0530

    Fix 1 for adsterra

--- src/Pages/Drinks/index.js
+++ src/Pages/Drinks/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Blog from '../../Components/Blog';
 import { latestDrinksDetails } from '../../Constants/appConstants';
 import '../Blogs/Blogs.css';
 import { Helmet } from 'react-helmet';
 
 function DrinksPage() {
+  useEffect(() => {
+    const script = document.createElement('script');
+
+    script.src =
+      '//pl17568951.highperformancegate.com/5fa170385178a32bef3ed45d5d529520/invoke.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <div className="blogsPageWrapper">
       <div className="blogsPageHeader">
@@ -26,14 +40,7 @@ function DrinksPage() {
           />
         );
       })}
-      <Helmet>
-        <script
-          async="async"
-          data-cfasync="false"
-          src="//pl17568951.highperformancegate.com/5fa170385178a32bef3ed45d5d529520/invoke.js"
-        ></script>
-        <div id="container-5fa170385178a32bef3ed45d5d529520"></div>
-      </Helmet>
+      <div id="container-5fa170385178a32bef3ed45d5d529520"></div>
     </div>
   );
 }
