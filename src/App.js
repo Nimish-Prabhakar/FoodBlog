ed245fc5a6e25e0bd9842c9e824ed1de864d8647
Nishu <nimish.prabhakar@eagleview.com>
2022-08-19 09:07:20 +0530

    removed ip logging

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Home from './Pages/Home';
@@ -8,25 +8,8 @@ import DrinksPage from './Pages/Drinks';
 import RecepiesPage from './Pages/Recepies';
 import PrivacyPolicy from './Pages/PrivacyPolicy';
 import About from './Pages/About';
-import axios from 'axios';
 
 function App() {
-  const [ip, setIP] = useState('');
-
-  //creating function to load ip address from the API
-  const getData = async () => {
-    const res = await axios.get('https://lumtest.com/myip.json');
-    console.log(res.data.country);
-    console.log(res.data.geo);
-    console.log(res.data.ip);
-    setIP(res.data.ip);
-  };
-
-  useEffect(() => {
-    //passing getData method to the lifecycle method
-    getData();
-  }, []);
-
   return (
     <BrowserRouter>
       <NavBar />
