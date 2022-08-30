25b7bd4ab1bd22c77a5d0949e816dab6c7c98b91
Nishu <nimish.prabhakar@eagleview.com>
2022-08-31 05:08:17 +0530

    added popup at drinks

--- src/Pages/Drinks/index.js
+++ src/Pages/Drinks/index.js
@@ -2,28 +2,35 @@ import React, { useEffect } from 'react';
 import Blog from '../../Components/Blog';
 import { latestDrinksDetails } from '../../Constants/appConstants';
 import '../Blogs/Blogs.css';
-import { Helmet } from 'react-helmet';
+import useScript from '../../Components/useScript';
 
 function DrinksPage() {
+  useScript(
+    'https://frenchequal.pro/code/pops.js?h=waWQiOjExMzAwNjUsInNpZCI6MTE1MzUyMywid2lkIjozNzE2OTIsInNyYyI6Mn0=eyJ',
+    'childElement'
+  );
+
   return (
     <div className="blogsPageWrapper">
-      <div className="blogsPageHeader">
+      <div id="parentElement" className="blogsPageHeader">
         <h3 className="latestBlogsTitle">Breathtaking Drinks</h3>
       </div>
       {latestDrinksDetails.map((blog, index) => {
         return (
-          <Blog
-            key={index}
-            image={blog.image}
-            date={blog.date}
-            user={blog.user}
-            para1={blog.para1}
-            para2={blog.para2}
-            para3={blog.para3}
-            type={blog.type}
-            header={blog.header}
-            comments={blog.comments}
-          />
+          <div id="childElement">
+            <Blog
+              key={index}
+              image={blog.image}
+              date={blog.date}
+              user={blog.user}
+              para1={blog.para1}
+              para2={blog.para2}
+              para3={blog.para3}
+              type={blog.type}
+              header={blog.header}
+              comments={blog.comments}
+            />
+          </div>
         );
       })}
     </div>
